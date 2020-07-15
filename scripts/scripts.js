bf541004edce72d8203464eac06f9ca0f47cdf97
Rmarins <rafaelcunha@email.com>
2020-07-15 00:46:47 -0300

    Usando Caixa de seleção para divs

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('#select').on('change',function(){
+
+        var selectValor = '#' + $(this).val();
+
+        $('#pai').children('div').hide();
+        $('#pai').children(selectValor).show();
+
+    });
+})
